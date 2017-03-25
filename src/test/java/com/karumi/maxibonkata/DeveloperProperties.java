333f983b5db807f5405014100353288c09c7263b
Cristian Monforte <cristian.monforte@xing.com>
2017-03-25 12:05:36 +0100

    first tests

---
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -0,0 +1,29 @@
+package com.karumi.maxibonkata;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnitQuickcheck.class) public class DeveloperProperties {
+
+    @Property(trials = 2)
+    public void theNumberOfMaxibonsAssignedIsPositiveOrZero(String name, int numberOfMaxibons) {
+        System.out.println(numberOfMaxibons);
+        Developer developer = new Developer(name, numberOfMaxibons);
+        System.out.println(developer);
+        assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
+    }
+
+    @Test public void numberOfMaxibons() throws Exception {
+        assertEquals(3, Karumies.PEDRO.getNumberOfMaxibonsToGrab());
+        assertEquals(1, Karumies.ALBERTO.getNumberOfMaxibonsToGrab());
+        assertEquals(0, Karumies.DAVIDE.getNumberOfMaxibonsToGrab());
+        assertEquals(1, Karumies.JORGE.getNumberOfMaxibonsToGrab());
+        assertEquals(2, Karumies.SERGIO.getNumberOfMaxibonsToGrab());
+    }
+}
